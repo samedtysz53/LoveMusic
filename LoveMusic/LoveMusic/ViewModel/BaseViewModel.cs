576589369c2fa174da1a2a20d32aea60c3fa10d9
samed tüysüz <49450735+samedtysz53@users.noreply.github.com>
2022-09-19 03:50:34 +0300

    Add project files.

---
+++ LoveMusic/LoveMusic/ViewModel/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoveMusic.ViewModel
+{
+    public class BaseViewModel
+    {
+    }
+}
---
+++ LoveMusic/LoveMusic/ViewModel/LandingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoveMusic.ViewModel
+{
+    public class LandingViewModel:BaseViewModel
+    {
+
+    }
+}
